6ed01e1902d6e0ba5256b851ecf9163bfe0d2ead
Vinh Tran <truongvinht@gmail.com>
2019-02-17 11:25:06 +0100

    update help for get

--- view/helpMessage.js
+++ view/helpMessage.js
@@ -22,7 +22,8 @@ const helpMessage = (PREFIX, author, hasRole) => {
         .setDescription(`${i18n.get('FollowingCommands')}`)
         .addField(`${PREFIX}ep`, `${i18n.get('InfoGuildMemberEPList')}`)
         .addField(`${PREFIX}find NAME`, `${i18n.get('InfoFindPlayer')}`)
-        .addField(`${PREFIX}time`, `${i18n.get('Time')}`);
+        .addField(`${PREFIX}time`, `${i18n.get('Time')}`)
+        .addField(`${PREFIX}get ATTACHMENT`, `${i18n.get('ImageParsing')}`);
         //.addField(`${PREFIX}afk [TEXT]`, `${i18n.get('InfoCheckoutPlayer')}`)
     
     if (hasRole) {
